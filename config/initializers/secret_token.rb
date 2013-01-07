73d4b4928da66f422529190a6996f8205462d5b6
David Sulc <davidsulc@gmail.com>
2013-01-07 19:50:00 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TuxyMat::Application.config.secret_token = 'c1b6ca40002a7159da89bac83b2af9a315a1dcfc05a8fbd78d478d26a40941d157f083d7cf7ad6d49a5a1a5dc8e6852400d4a4579df4c0e7a6a7c772f83d5df7'
