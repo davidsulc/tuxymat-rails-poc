e7131264f110e947ded8efc6c7326040dfd2c570
David Sulc <davidsulc@gmail.com>
2013-01-08 19:43:17 +0100

    configure gems

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,16 @@ module TuxyMat
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.generators do |g|
+      g.test_framework :rspec, :fixture => true
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+      g.form_builder :simple_form
+      g.template_engine :haml
+    end
+    
+    # autoload lib directory
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
   end
 end
