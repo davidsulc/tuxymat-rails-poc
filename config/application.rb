ec51aa63c11dbf78e13abaf94f1143b242528740
David Sulc <davidsulc@gmail.com>
2013-01-08 22:16:18 +0100

    don't declare simple_form as the form builder generator
    causes an error when generating scaffolding

--- config/application.rb
+++ config/application.rb
@@ -68,7 +68,6 @@ module TuxyMat
     config.generators do |g|
       g.test_framework :rspec, :fixture => true
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
-      g.form_builder :simple_form
       g.template_engine :haml
     end
     
