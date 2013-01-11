5e61f4dc1d6cf88025a215b54e26fef6773fe95b
David Sulc <davidsulc@gmail.com>
2013-01-11 23:39:00 +0100

    fix price display

--- app/helpers/vending_machines_helper.rb
+++ app/helpers/vending_machines_helper.rb
@@ -1,2 +1,6 @@
 module VendingMachinesHelper
+  # format a price for display (e.g. 12345.5 => 12'345.50)
+  def pretty_price_format(price)
+    number_with_precision(price, precision: 2, delimiter: "'")
+  end
 end
