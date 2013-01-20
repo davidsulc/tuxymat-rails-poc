ac1b9d4671d6a30f6a642190a00136f2bcf8781f
David Sulc <davidsulc@gmail.com>
2013-01-20 14:03:50 +0100

    partner's name should be present and unique

--- spec/models/partner_spec.rb
+++ spec/models/partner_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Partner do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "should have a valid factory" do
+    FactoryGirl.build(:partner).should be_valid
+  end
+
+  it "should require a name number" do
+    FactoryGirl.build(:partner, :name => "").should_not be_valid
+  end
+
+  it "should require the name to be unique" do
+    FactoryGirl.create(:partner, :name => "John Doe")
+    FactoryGirl.build(:partner, :name => "John Doe").should_not be_valid
+  end
+  
 end
