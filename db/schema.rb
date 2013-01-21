0add19398cf7739a7c283e976a8b3debd39b889f
David Sulc <davidsulc@gmail.com>
2013-01-21 21:45:29 +0100

    add is_admin flag to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130120160250) do
+ActiveRecord::Schema.define(:version => 20130121204336) do
 
   create_table "contracts", :force => true do |t|
     t.date     "start_date"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20130120160250) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.boolean  "is_admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
